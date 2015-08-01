63758d3994cd4aef596002a782158adc67a49b73
camus <camus@SHAKA>
2015-07-31 22:06:37 -0500

    initial commit

---
+++ hibernate-xml/src/main/java/org/neos/hibernate/domain/Region.java
@@ -0,0 +1,21 @@
+package org.neos.hibernate.domain;
+
+public class Region {
+	
+	private int idRegion;
+	private String nameRegion;
+	public int getIdRegion() {
+		return idRegion;
+	}
+	public void setIdRegion(int idRegion) {
+		this.idRegion = idRegion;
+	}
+	public String getNameRegion() {
+		return nameRegion;
+	}
+	public void setNameRegion(String nameRegion) {
+		this.nameRegion = nameRegion;
+	}
+	
+
+}
---
+++ hibernate-xml/src/main/java/org/neos/hibernate/test/HibernateRegion.java
@@ -0,0 +1,49 @@
+package org.neos.hibernate.test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.neos.hibernate.domain.Region;
+
+public class HibernateRegion {
+
+	private static SessionFactory factory;
+
+	public static void main(String[] args) {
+
+		factory = new Configuration().configure().buildSessionFactory();
+		HibernateRegion manager = new HibernateRegion();
+
+		manager.listRegions();
+		factory.close();
+	}
+
+	public void listRegions() {
+
+		Session session = factory.openSession();
+		Transaction txt = null;
+		try {
+
+			txt = session.beginTransaction();
+			@SuppressWarnings("unchecked")
+			List<Region> ltsRegions = session.createQuery("FROM Region").list();
+			for (Region region : ltsRegions) {
+				System.out.println("IdRegion: " + region.getIdRegion());
+				System.out.println(" Name: " + region.getNameRegion());
+			}
+			txt.commit();
+		} catch (HibernateException e) {
+			txt.rollback();
+		} finally {
+			session.close();
+			System.out.println("CERRANDO CONEXION");
+		}
+
+	}
+
+}
