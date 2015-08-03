e4d58168c8b315aab6d5be855efe661b81dcb397
hhugohm <hhugohm@gmail.com>
2015-08-03 10:08:22 -0500

    se cambia mensaje en el servicio

--- axis1-webservice/src/org/neos/axis1/ws/service/BusinessService.java
+++ axis1-webservice/src/org/neos/axis1/ws/service/BusinessService.java
@@ -5,7 +5,7 @@ public class BusinessService {
 	public String getMessageService(String name){
 		System.out.println("entrando en el webrsevice...");
 		
-		return "HELLO "+ name + " TO WEBSERVICE ";
+		return "HELLO "+ name + " WELCOME TO WEBSERVICE AXIS1 ";
 		
 	}
 
