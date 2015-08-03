f68cd663b06ca391e707103eaaa7aae1faa8695e
hhugohm <hhugohm@gmail.com>
2015-08-03 10:30:52 -0500

    se modifica servicio

--- axis1-webservice/src/org/neos/axis1/ws/service/BusinessService.java
+++ axis1-webservice/src/org/neos/axis1/ws/service/BusinessService.java
@@ -1,11 +1,18 @@
 package org.neos.axis1.ws.service;
 
+/**
+ * 
+ * @author Hector Hugo Hidalgo
+ * 03/08/2015
+ *
+ */
 public class BusinessService implements IBusinessService {
 	
+	@Override
 	public String getMessageService(String name){
 		System.out.println("entrando en el webrsevice...");
 		
-		return "HELLO "+ name + " WELCOME TO WEBSERVICE AXIS1 ";
+		return "HELLO "+ name + "!, WELCOME TO WEBSERVICE AXIS1 ";
 		
 	}
 
