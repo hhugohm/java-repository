62b601f5109c36f754a47dad54da13275a651802
hhugohm <hhugohm@gmail.com>
2015-08-12 13:04:26 -0500

    cambio en el contexto del servicio web

--- axis1-clientm/src/main/java/org/neos/axis1/ws/test/Axis1Test.java
+++ axis1-clientm/src/main/java/org/neos/axis1/ws/test/Axis1Test.java
@@ -9,7 +9,7 @@ public class Axis1Test {
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException {
 		
-		java.net.URL endPointURL= new java.net.URL("http://localhost:8080/axis1-webservice/services/BusinessService");
+		java.net.URL endPointURL= new java.net.URL("http://localhost:8080/axis1m-webservice/services/BusinessService");
 		
 		org.apache.axis.client.Service service = new org.apache.axis.client.Service();
 		BusinessServiceSoapBindingStub stub = new BusinessServiceSoapBindingStub(endPointURL,service);
