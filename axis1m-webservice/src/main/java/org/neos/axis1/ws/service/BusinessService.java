52e62110788b7356ef549391b38f3d85820376d4
camus <camus@SHAKA>
2015-08-12 13:01:43 -0500

    load init

---
+++ axis1m-webservice/src/main/java/org/neos/axis1/ws/service/BusinessService.java
@@ -0,0 +1,19 @@
+package org.neos.axis1.ws.service;
+
+/**
+ * 
+ * @author Hector Hugo Hidalgo
+ * 03/08/2015
+ *
+ */
+public class BusinessService implements IBusinessService {
+	
+	@Override
+	public String getMessageService(String name){
+		System.out.println("entrando en el webrsevice...");
+		
+		return "HELLO "+ name + "!, WELCOME TO WEBSERVICE AXIS1 hola";
+		
+	}
+
+}
---
+++ axis1m-webservice/src/main/java/org/neos/axis1/ws/service/IBusinessService.java
@@ -0,0 +1,12 @@
+package org.neos.axis1.ws.service;
+
+/**
+ * 
+ * @author Hector Hugo Hidalgo
+ * 03/08/2015
+ *
+ */
+public interface IBusinessService {
+
+	public String getMessageService(String name);
+}
