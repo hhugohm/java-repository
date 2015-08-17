427dec2fd47f622a54f40749a550eff16b5a659e
hhugohm <hhugohm@gmail.com>
2015-08-16 20:52:07 -0500

    nueva clases test

---
+++ axis1-clientm/src/main/java/org/neos/axis1/ws/test/TestTemperatura.java
@@ -0,0 +1,23 @@
+package org.neos.axis1.ws.test;
+
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import org.neos.axis1.ws.service.BusinessServiceServiceLocator;
+import org.neos.axis1.ws.service.BusinessService_PortType;
+
+public class TestTemperatura {
+
+	public static void main(String[] args) throws ServiceException, 
+															 RemoteException {
+
+		BusinessServiceServiceLocator locator = 
+											new BusinessServiceServiceLocator();
+		BusinessService_PortType server = locator.getBusinessService();
+
+		System.out.println("25.5 grados Celcius= " + 
+						server.celsiusToFarenheit(25.5f) + " grados Farenheit");
+	}
+
+}
