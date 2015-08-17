086c2fc118b6d796876d47c52bb01df831788e5c
hhugohm <hhugohm@gmail.com>
2015-08-16 21:39:30 -0500

    cambio en implementacion

--- axis1m-webservice/src/main/java/org/neos/axis1/ws/service/BusinessService.java
+++ axis1m-webservice/src/main/java/org/neos/axis1/ws/service/BusinessService.java
@@ -8,23 +8,24 @@ package org.neos.axis1.ws.service;
  */
 public class BusinessService implements IBusinessService {
 	
+	@Override
 	public String getMessageService(String name){
 		System.out.println("entrando en el webrsevice...");
 		
 		return "HELLO "+ name + "!, WELCOME TO WEBSERVICE AXIS1 hola";
 		
 	}
-	
+	@Override
 	public float celsiusToFarenheit(float celsius) {
 		return (celsius * 1.8f+32);
 	}
 
-	
+	@Override
 	public float farenheitToCelsius(float farenheit) {
 		return ((farenheit -32)* 5/9);
 	}
 	
-	
+	@Override
 	public double factorial(int number) {
 		 if (number <= 1)
 	            return 1;
