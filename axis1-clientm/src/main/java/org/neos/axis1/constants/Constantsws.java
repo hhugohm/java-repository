d4cea2a56a269b528a2bd87f8b6a72919ea40208
hhugohm <hhugohm@gmail.com>
2015-09-23 10:27:55 -0500

    nueva clase de constantes

---
+++ axis1-clientm/src/main/java/org/neos/axis1/constants/Constantsws.java
@@ -0,0 +1,10 @@
+package org.neos.axis1.constants;
+
+public class Constantsws {
+	
+	public static final String wsdl_businessService=
+		"http://localhost:8080/axis1m-webservice/services/BusinessService?wsdl";
+	
+	
+
+}
