1dac10b1cd616f9d3f1a0d6d5d50c125a46a3edb
hhugohm <hhugohm@gmail.com>
2015-09-25 13:19:46 -0500

    cambios emplemetacion

--- patterns/src/main/java/org/neos/patterns/dao/Employee.java
+++ patterns/src/main/java/org/neos/patterns/dao/Employee.java
@@ -2,37 +2,49 @@ package org.neos.patterns.dao;
 
 public class Employee {
 	
-	
-
-	private int id_employee;
+	private int idEmployee;
 	private String name;
-	private String last_name;
+	private String lastName;
 	
+	public Employee(){}
 	
-	public Employee(int id_employee,String name,String last_name){
-		this.id_employee=id_employee;
+	public Employee(int idEmployee,String name,String lastName){
+		this.idEmployee=idEmployee;
 		this.name=name;
-		this.last_name=last_name;
+		this.lastName=lastName;
 		
 	}
-	
-	public int getId_employee() {
-		return id_employee;
+
+
+	public int getIdEmployee() {
+		return idEmployee;
 	}
-	public void setId_employee(int id_employee) {
-		this.id_employee = id_employee;
+
+
+	public void setIdEmployee(int idEmployee) {
+		this.idEmployee = idEmployee;
 	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getLast_name() {
-		return last_name;
+
+
+	public String getLastName() {
+		return lastName;
 	}
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
+	
+	
 		
 }
--- patterns/src/main/java/org/neos/patterns/dao/test/DaoTest.java
+++ patterns/src/main/java/org/neos/patterns/dao/test/DaoTest.java
@@ -2,19 +2,22 @@ package org.neos.patterns.dao.test;
 
 import org.neos.patterns.dao.Employee;
 import org.neos.patterns.dao.EmployeeDao;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DaoTest {
 
 	public static void main(String[] args) {
-		ApplicationContext context = 
-	    		new ClassPathXmlApplicationContext("spring-module.xml");
+		 ClassPathXmlApplicationContext context =
+				 new ClassPathXmlApplicationContext("spring-module.xml");
 		
-		EmployeeDao dao = (EmployeeDao) context.getBean("employeeDAO");
+		EmployeeDao dao = (EmployeeDao) context.getBean("employeeDao");
 		
 		Employee employee= dao.getEmployeeById(1);
-		System.out.println(employee.getId_employee() + employee.getName()+employee.getLast_name());
+		System.out.println("Employee: "+ employee.getIdEmployee() 
+		+ " "+employee.getName()
+		+ " "+employee.getLastName());
+		
+		context.close();
 
 	}
 
