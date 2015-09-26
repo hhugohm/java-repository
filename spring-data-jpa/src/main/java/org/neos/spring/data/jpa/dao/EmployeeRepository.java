365059857a8526886a8813587d71d71840605a7f
hhugohm <hhugohm@gmail.com>
2015-09-25 23:16:33 -0500

    se hace cambio en el repositorio

--- spring-data-jpa/src/main/java/org/neos/spring/data/jpa/dao/EmployeeRepository.java
+++ spring-data-jpa/src/main/java/org/neos/spring/data/jpa/dao/EmployeeRepository.java
@@ -2,23 +2,33 @@ package org.neos.spring.data.jpa.dao;
 
 import java.util.List;
 
+
 import org.neos.spring.data.jpa.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly=true)
 public interface EmployeeRepository extends  JpaRepository<Employee, Integer>{
 
-	public Employee findByidEmployee(Integer id);
+	public Employee findByidEmployee(int id);
 	
-	@Query("SELECT e FROM Employee e WHERE e.name = ?")
+	@SuppressWarnings("el-syntax")
+	@Query("SELECT e FROM #{#entityName} e WHERE e.name = ?")
 	public List<Employee> findByname(String name);
 	
 	
 	@Query("SELECT e FROM Employee e WHERE e.name  LIKE CONCAT('%',:name,'%') ORDER BY e.name")
 	public List<Employee> findByEmployeeWithPartOfName(@Param("name") String name);
+	
+	@Modifying
+	@Transactional(timeout = 10)
+	@Query("UPDATE Employee e set e.name = ?1, e.lastName = ?2 where e.idEmployee = ?3")
+	void setEmployeeInfoById(String name, String lastName, Integer idEmployee);
 	
 
 }
