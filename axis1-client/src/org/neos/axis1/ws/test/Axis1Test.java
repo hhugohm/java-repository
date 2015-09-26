a4eb6ada52f6f9cc4fea27b870f04236a0968d69
hhugohm <hhugohm@gmail.com>
2015-09-25 21:24:09 -0500

    cambio de variables

--- axis1-client/src/org/neos/axis1/ws/test/Axis1Test.java
+++ axis1-client/src/org/neos/axis1/ws/test/Axis1Test.java
@@ -3,6 +3,7 @@ package org.neos.axis1.ws.test;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 
+import org.apache.axis.client.Service;
 import org.neos.axis1.ws.service.BusinessServiceSoapBindingStub;
 
 public class Axis1Test {
@@ -11,8 +12,9 @@ public class Axis1Test {
 		
 		java.net.URL endPointURL= new java.net.URL("http://localhost:8080/axis1-webservice/services/BusinessService");
 		
-		org.apache.axis.client.Service service = new org.apache.axis.client.Service();
-		BusinessServiceSoapBindingStub stub = new BusinessServiceSoapBindingStub(endPointURL,service);
+		Service service = new Service();
+		BusinessServiceSoapBindingStub stub = 
+						new BusinessServiceSoapBindingStub(endPointURL,service);
 		System.out.println(stub.getMessageService("HUGO"));
 		
 	}
